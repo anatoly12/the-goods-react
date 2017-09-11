51b550b36c7033de92ca5f6ae2d6338c195bc2b4
Anatoly P <anatolypolicastroa@gmail.com>
2017-09-11 14:30:42 -0500

    Save before trying to figure out deployed version problem with the group.

--- app/components/children/Home.js
+++ app/components/children/Home.js
@@ -9,12 +9,39 @@ var Home = React.createClass({
 
    		return (
         <div>
-     			<div>
-     				<p>
-             	<Link to="/Home/RentedItems"><button className="btn btn-primary btn-lg">Their Goods</button></Link>
-              <Link to="/Home/YourItems"><button className="btn btn-danger btn-lg">Your Goods</button></Link>
-            </p>
-     			</div>
+          <div className="addItemFormSheet" hide>
+            <Form action="/api/createItem" method="POST" class="createItemForm">
+                Item Name:
+                <br>
+                <input type="text" name="itemName" >
+                <br> Category:
+                <br>
+                <input type="text" name="category" >
+                <br> Owner:
+                <br>
+                <input type="text" name="owner" >
+                <br>
+                <br> Location:
+                <br>
+                <input type="text" name="location" >
+                <br> Price:
+                <br>
+                <input type="text" name="price" >
+                <br> Image:
+                <br>
+                <input type="text" name="image" >
+                <br> Availability:
+                <br>
+                <input type="text" name="availability" >
+                <br> Earned:
+                <br>
+                <input type="text" name="earned" >
+                <br>
+                <br>
+                <input type="submit" value="Submit">
+
+            <Form />
+          </div>
      			<div>
      				{this.props.children}
   			  </div>
