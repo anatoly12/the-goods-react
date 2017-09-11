6d4efb894c38afcdb583efda468b89964750a871
Anatoly P <anatolypolicastroa@gmail.com>
2017-09-11 17:24:12 -0500

    Styled and designed the Home add a item form

--- app/components/children/Home.js
+++ app/components/children/Home.js
@@ -2,53 +2,61 @@ var React = require("react");
 // Including the Link component from React Router to navigate within our application without full page reloads
 var Link = require("react-router").Link;
 
+import { Container, Row, Col, Well, Media, Thumbnail, Button, Grid  } from 'react-bootstrap';
+
+
 var Home = React.createClass({
 
-  	// Here we render the component
-  	render: function() {
+  // Here we render the component
+  render: function() {
 
-   		return (
-        <div>
-          <div className="addItemFormSheet" hide>
-            <Form action="/api/createItem" method="POST" class="createItemForm">
-                Item Name:
-                <br>
-                <input type="text" name="itemName" >
-                <br> Category:
-                <br>
-                <input type="text" name="category" >
-                <br> Owner:
-                <br>
-                <input type="text" name="owner" >
-                <br>
-                <br> Location:
-                <br>
-                <input type="text" name="location" >
-                <br> Price:
-                <br>
-                <input type="text" name="price" >
-                <br> Image:
-                <br>
-                <input type="text" name="image" >
-                <br> Availability:
-                <br>
-                <input type="text" name="availability" >
-                <br> Earned:
-                <br>
-                <input type="text" name="earned" >
-                <br>
-                <br>
-                <input type="submit" value="Submit">
+    return (
+      <Grid>
+        <Row>
+          <Col style={{marginRight: "10%", marginLeft: "10%"}}>
+            <form action="/api/createItem" method="POST" className="createItemForm">
+              Item Name:
+              <br/>
+              <input className="form-control" type="text" name="itemName" value=""/>
+              <br/>
+              Category:
+              <br/>
+              <input className="form-control" type="text" name="category" value=""/>
+              <br/>
+              Owner:
+              <br/>
+              <input className="form-control" type="text" name="owner" value=""/>
+              <br/>
+              <br/>
+              Location:
+              <br/>
+              <input className="form-control" type="text" name="location" value=""/>
+              <br/>
+              Price:
+              <br/>
+              <input className="form-control" type="text" name="price" value=""/>
+              <br/>
+              Image:
+              <br/>
+              <input className="form-control" type="text" name="image" value=""/>
+              <br/>
+              Availability:
+              <br/>
+              <input className="form-control" type="text" name="availability" value=""/>
+              <br/>
+              Earned:
+              <br/>
+              <input className="form-control" type="text" name="earned" value=""/>
+              <br/>
+              <br/>
+              <input type="submit" value="Submit" className="btn btn-primary"/>
+            </form>
+          </Col>
+        </Row>
+      </Grid>
 
-            <Form />
-          </div>
-     			<div>
-     				{this.props.children}
-  			  </div>
-        </div>
-		);
-  	}
+    );
+  }
 });
 
-// Export the component back for use in other files
 module.exports = Home;
