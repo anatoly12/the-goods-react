32ba0bca7b6935271336e66e6ba7ec81190ff0df
Anatoly P <anatolypolicastroa@gmail.com>
2017-09-11 17:57:46 -0500

    Finished the Home tab Add an item form and styling

--- app/components/children/Home.js
+++ app/components/children/Home.js
@@ -2,51 +2,54 @@ var React = require("react");
 // Including the Link component from React Router to navigate within our application without full page reloads
 var Link = require("react-router").Link;
 
-import { Container, Row, Col, Well, Media, Thumbnail, Button, Grid  } from 'react-bootstrap';
+import { Container, Row, Col, Button, Grid  } from 'react-bootstrap';
 
 
 var Home = React.createClass({
+
 
   // Here we render the component
   render: function() {
 
     return (
+
       <Grid>
         <Row>
           <Col style={{marginRight: "10%", marginLeft: "10%"}}>
+
             <form action="/api/createItem" method="POST" className="createItemForm">
               Item Name:
               <br/>
-              <input className="form-control" type="text" name="itemName" value=""/>
+              <input  className="form-control" type="text" name="itemName"  />
               <br/>
               Category:
               <br/>
-              <input className="form-control" type="text" name="category" value=""/>
+              <input className="form-control" type="text" name="category" />
               <br/>
               Owner:
               <br/>
-              <input className="form-control" type="text" name="owner" value=""/>
+              <input className="form-control" type="text" name="owner" />
               <br/>
               <br/>
               Location:
               <br/>
-              <input className="form-control" type="text" name="location" value=""/>
+              <input className="form-control" type="text" name="location" />
               <br/>
               Price:
               <br/>
-              <input className="form-control" type="text" name="price" value=""/>
+              <input className="form-control" type="text" name="price" />
               <br/>
               Image:
               <br/>
-              <input className="form-control" type="text" name="image" value=""/>
+              <input className="form-control" type="text" name="image" />
               <br/>
               Availability:
               <br/>
-              <input className="form-control" type="text" name="availability" value=""/>
+              <input className="form-control" type="text" name="availability" />
               <br/>
               Earned:
               <br/>
-              <input className="form-control" type="text" name="earned" value=""/>
+              <input className="form-control" type="text" name="earned" v/>
               <br/>
               <br/>
               <input type="submit" value="Submit" className="btn btn-primary"/>
@@ -54,6 +57,8 @@ var Home = React.createClass({
           </Col>
         </Row>
       </Grid>
+
+
 
     );
   }
